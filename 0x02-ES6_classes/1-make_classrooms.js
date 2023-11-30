272ced8db895427f34e7a3922b1458c8e749e540
Markkelvin14 <mjkelvin38@gmail.com>
2023-11-30 11:44:28 +0000

    ES6_classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,6 +3,6 @@ export default initializeRooms;
 
 function initializeRooms() {
   const sizes = [19, 20, 34];
-  const rooms = sizes.map(size => new ClassRoom(size));
+  const rooms = sizes.map((size) => new ClassRoom(size));
   return rooms;
 }
